b1f84ae1159c2a6899681b86a1d46c1b7f0abd25
Billy Robert O'Neal III <bion@microsoft.com>
2021-11-30 10:10:45 -0800

    Fix build failure and Robert code review feedback.

--- include/vcpkg/archives.h
+++ include/vcpkg/archives.h
@@ -10,6 +10,4 @@ namespace vcpkg
     void extract_tar(const Path& tar_tool, const Path& archive, const Path& to_path);
     // Extract `archive` to `path`, deleting `path` first.
     void extract_archive(const VcpkgPaths& paths, const Path& archive, const Path& to_path);
-    // Extract `archive` to a subdirectory of `path`, returning the path to the temporary subdirectory.
-    Path extract_archive_to_temp_subdirectory(const VcpkgPaths& paths, const Path& archive, const Path& to_path);
 }
